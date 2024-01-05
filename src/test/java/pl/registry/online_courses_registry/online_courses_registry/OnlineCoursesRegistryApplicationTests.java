3e6d0f90c9ac1005cf033519eeebd8dcbee1adb7
Windows <00Zbiewski@gmail.com>
2024-01-05 16:10:16 +0100

    Init

---
+++ src/test/java/pl/registry/online_courses_registry/online_courses_registry/OnlineCoursesRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.registry.online_courses_registry.online_courses_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoursesRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
